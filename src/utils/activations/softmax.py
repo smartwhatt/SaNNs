fix: normalize value to below 0 before aplying softmax

src/utils/activations/softmax.py
@@ -8,4 +8,4 @@ def softmax(x):
         x: np.ndarray
             The input to the activation function.
     """
-    return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+    return np.exp(x) / np.sum(np.exp(x - np.max(x, axis=1, keepdims=True)), axis=1, keepdims=True)

src/utils/layers/dense.py
@@ -1,7 +1,7 @@
 import jax.numpy as np
 import jax.random as random
 from jax import grad, jit, vmap
-from ..activations import *
+from ..activations import linear
 
 
 class Dense:
@@ -14,28 +14,33 @@ class Dense:
         n_neuron: Int
             The number of neurons in the layer.
         activation: callable
-            The activation function to use. (default: linear (lambda x:x))
-        seed: int
+            The activation function to use. (default: linear)
+        seed: Int
             The seed to use for the random number generator. (default: 12321)
     """
 
-    def __init__(self, n_input, n_neuron, activation=linear, seed=12321):
+    def __init__(self, n_input, n_neuron, activation=linear, seed=12321, scale=0.01):
         self.key = random.PRNGKey(seed)
         self.n_input = n_input
         self.n_neuron = n_neuron
 
-        self.w = 0.10 * random.normal(self.key, (n_input, n_neuron))
+        self.w = scale * random.normal(self.key, (n_input, n_neuron))
         self.b = np.zeros((1, n_neuron))
-        
+
         self.activation = jit(activation)
-        self.calculate_layer = jit(lambda inputs, weights, bias: np.dot(inputs, weights) + bias)
 
         self.forward = jit(self.forward)
 
     def forward(self, x):
-        self.output =  self.activation(self.calculate_layer(x, self.w, self.b))
+        self.output = self.activation(np.dot(x, self.w) + self.b)
 
         return self.output
 
+    def as_pytree(self):
+        return [self.w, self.b]
+
     def __call__(self, x):
         return self.forward(x)
+
+    def __repr__(self):
+        return f"Dense(n_input={self.n_input}, n_neuron={self.n_neuron}, activation={self.activation})"